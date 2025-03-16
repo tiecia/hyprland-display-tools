b34f10a2da9e98fc98ade9e506788116ca54f70c
tiecia <ty.cia@outlook.com>
2025-03-16 10:57:25 -0700

    add command

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use hyprland::event_listener::{EventListener, MonitorAddedEventData};
+use std::process::Command;
 
 fn main() -> hyprland::Result<()> {
     let mut event_listener = EventListener::new();
@@ -17,9 +18,29 @@ fn on_monitor_added(data: MonitorAddedEventData) {
     let monitor_id = data.id;
     let monitor_description = data.description;
     let monitor_name = data.name;
-    println!("Monitor connected: {monitor_name}, {monitor_id}, {monitor_description}")
+    println!("Monitor connected: {monitor_name}, {monitor_id}, {monitor_description}");
+
+    reset_bar();
 }
 
 fn on_monitor_removed(monitor_name: String) {
-    println!("Monitor disconnected: {monitor_name}")
+    println!("Monitor disconnected: {monitor_name}");
+
+    reset_bar();
+}
+
+fn reset_bar() {
+    close_bar();
+    open_bar();
+}
+
+fn close_bar() {
+    Command::new("ags")
+        .arg("-q")
+        .status()
+        .expect("Unable to close bar.");
+}
+
+fn open_bar() {
+    Command::new("ags").spawn().expect("Unable to open bar.");
 }
